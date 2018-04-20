1495fb41c1465d776110944a98c8af6f60b83fb3
Your Name <378029633@qq.com>
2018-04-20 11:29:31 +0800

    更新

--- dist_serve/static.js
+++ dist_serve/static.js
@@ -1,7 +1,29 @@
 var koa = require('koa')
 var app = new koa();
 var serve = require('koa-static');
-app.use(serve(__dirname+'./../dist',{maxage:3153600000}))
+const path = require('path');
+const render = require('koa-ejs');
+render(app, {
+    root: path.join(__dirname+'./../dist'),
+    layout: false,
+    viewExt: 'html',
+    cache: false,
+    debug: false
+  });
+var koaRouter = require('koa-router')
+
+const router = new koaRouter();
+router.get('*',async function (ctx,next) {
+    // debugger
+    if(ctx.URL.pathname.indexOf('.')!="-1"){
+        await next()
+    }else{
+        await ctx.render('index')
+    }
+    
+},serve(__dirname+'./../dist',{maxage:3153600000}))
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(8300);
 
 console.log('listening on port 8300');
